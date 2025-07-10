c2ad9c23deaeb320ac0cd2f8751ad4f62a6e4b93
XiaoYou <34185185+zhanghuiyou@users.noreply.github.com>
2025-07-10 22:32:11 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -150,7 +150,7 @@ async function initConfig() {
           }
           adminConfig = {
             SiteConfig: {
-              SiteName: process.env.SITE_NAME || 'MoonTV',
+              SiteName: process.env.SITE_NAME || 'XY TV',
               Announcement:
                 process.env.NEXT_PUBLIC_ANNOUNCEMENT ||
                 '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
